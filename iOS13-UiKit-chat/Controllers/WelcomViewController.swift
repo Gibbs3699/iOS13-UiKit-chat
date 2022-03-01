b5af2a16aeb78d8d1a6059779c840a8231149e35
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-03-01 21:33:23 +0700

    Adding typing animations, timers and for loops

--- iOS13-UiKit-chat/Controllers/WelcomViewController.swift
+++ iOS13-UiKit-chat/Controllers/WelcomViewController.swift
@@ -13,10 +13,21 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        titleLabel.text = ""
+        var charIndex = 0.0 //add .0 change to double
+        
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            print(charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
        
     }
     
-
 }
 
